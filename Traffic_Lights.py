754932b8fa77f88a8b25b114e6f10b1250030d45
Maisara Fatima <145922122+Maisara-fatima@users.noreply.github.com>
2023-12-25 19:45:28 +0000

    Create Traffic_Lights.py
    Setting a time of  when the color LED light illuminates

---
+++ Traffic_Lights.py
@@ -0,0 +1,21 @@
+import utime
+
+led_red = machine.Pin(15, machine.Pin.OUT)
+led_yellow = machine.Pin(14, machine.Pin.OUT)
+led_green = machine.Pin(13, machine.Pin.OUT)
+
+while True:
+    
+    led_red.value(1)
+    utime.sleep(3)
+    led_yellow.value(1)
+    utime.sleep(2)
+    led_red.value(0)
+    led_yellow.value(0)
+    led_green.value(1)
+    utime.sleep(3)
+    led_green.value(0)
+    led_yellow.value(1)
+    utime.sleep(3)
+    led_yellow.value(0)
+    
